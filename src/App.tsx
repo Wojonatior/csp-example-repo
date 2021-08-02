6ef817ca7bc823705fc758dc45e4b9f875446160
Jarek Wojciechowski <jarekwojo@gmail.com>
2021-08-02 12:12:02 -0400

    Add simple @react-google-maps/api example

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import './App.css';
+
+const containerStyle = {
+  width: "400px",
+  height: "400px"
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function MapComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "PUT_API_KEY_HERE"
+  });
+
+  const [map, setMap] = React.useState<any>(null);
+
+  const onLoad = React.useCallback((loadedMap: any) => {
+    //@ts-ignore
+    const bounds = new window.google.maps.LatLngBounds();
+    map?.fitBounds(bounds);
+    setMap(loadedMap);
+  }, [map]);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={ containerStyle }
+      center={ center }
+      zoom={ 10 }
+      onLoad={ onLoad }
+      onUnmount={ onUnmount }
+    >
+      {/* Child components, such as markers, info windows, etc. */ }
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapComponent />
     </div>
   );
 }
